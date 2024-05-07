bec7aece7888726bfc64810deb29e75014bdec8a
Mohamed-Nour-Bessadok <mohamednour.bessadok@medtech.tngit config --global user.email>
2024-05-07 23:17:51 +0100

    moved pages out of fingpt for front structure

--- fingpt/add_predictions.py
+++ fingpt/add_predictions.py
@@ -13,17 +13,12 @@ for element in elements:
 
     current_db = pd.read_csv(f'../docs/{element}_stock_extrapolated.csv')
     
-    # Remove non-numeric columns
     current_db.drop(columns=['Unnamed: 0'], inplace=True)
     
-    # Convert DataFrame to DMatrix
     dmatrix = xgb.DMatrix(data=current_db)
     
-    # Make predictions
     predictions = predict(element, start_date, end_date, dmatrix)
     
-    # Assign predictions to the DataFrame
     current_db['Adj Close'] = predictions
     
-    # Save DataFrame to CSV, overwriting the existing file
     current_db.to_csv(f'../docs/{element}_stock_extrapolated.csv', index=False, mode='w')
